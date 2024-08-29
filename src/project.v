17c73b8be56bc9f91c1852000445e2e19be7eb6c
Anulekhaka <anulekhaka@gmail.com>
2024-08-29 14:34:43 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,13 +17,13 @@ module tt_um_vlc (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
   assign uo_out [7:6]= 0;
 
   
-    vlc vlc(
+    vlc vlc1(
         .Turn_Left(ui_in[0]),
         .Turn_Right(ui_in[1]),
         .Emergency(ui_in[2]),
